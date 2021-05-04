efcf7b9ec947d05e989f2059b7f02aaa5cc64c5a
Assaf Oron <assaf.oron@gmail.com>
2021-05-03 18:27:37 -0700

    Update genutils.r

--- R/genutils.r
+++ R/genutils.r
@@ -6,11 +6,11 @@ uniroot(f=function(x,kay,you,ell,bee1,bee2) {bee1*pbinom(q=ell,size=kay,prob=x)+
 
 ### K-in-a-row targets
 
-k2targ<-function(k,hiend=TRUE)
+k2targ<-function(k,hitarg=TRUE)
 {
 if(k!=round(k) || k<=0) stop("k can only be a natural number.")
 tmp=0.5^(1/k)
-if(hiend) return(tmp)
+if(hitarg) return(tmp)
 1-tmp
 }
 
@@ -22,6 +22,6 @@ if(!hi) targ=1-targ
 kcand=-1/log2(targ)
 krange=(floor(kcand-expand):ceiling(kcand+expand))
 krange=krange[krange>0]
-data.frame(k=krange,balancePt=k2targ(krange,hiend=hi))
+data.frame(k=krange,balancePt=k2targ(krange,hitarg=hi))
 }
 
