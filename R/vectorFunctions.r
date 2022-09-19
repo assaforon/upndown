7b66e90a5abcb8b28c5958152fd2671d1a61bdda
Assaf Oron <assaf.oron@gmail.com>
2022-09-18 18:06:48 -0700

    Update vectorFunctions.r

--- R/vectorFunctions.r
+++ R/vectorFunctions.r
@@ -1,27 +1,55 @@
 #' Key Probability Vectors of Up-and-Down Designs
 #'
-#' Asymptotic and progression
+#' Dose-allocation probability vectors that quantify the instantaneous, cumulative, and asymptotic behavior of Up-and-Down designs.
 #'
 #' @details
-#' Up-and-Down designs (UDDs) generate random walk behavior, whose theoretical properties can be summarized via a transition probability matrix (TPM). Given the number of doses \eqn{M}, and the value of the cdf \eqn{F} at each dose (i.e., the positive-response probabilities), the specific UDD rules uniquely determine the TPM.
+#' Up-and-Down designs (UDDs) generate random walk behavior, which concentrates doses around the target quantile. Asymptotically, dose allocations follow a stationary distribution \eqn{\boldsymbol{\pi}} which can be calculated given the number of doses \eqn{M}, and the value of the cdf \eqn{F} at each dose (i.e., the positive-response probabilities), and the specific UDD rules. No matter the starting dose, the allocation distribution converges to \eqn{\boldsymbol{\pi}} at a geometric rate (Diaconis and Stroock, 1991).  
+#' 
+#' Three functions are offered:
+#' 
+#'  - `pivec` returns \eqn{\boldsymbol{\pi}}.
+#'  - `currentvec` returns the current (instantaneous) allocation distribution at step `n`, using the formula from Diaconis and Stroock (1991). 
+#'  - `cumulvec` returns the *cumulative* allocations, i.e., the expected proportions (or counts) of allocations during the experiment after `n` observations. This function is perhaps of greatest practical use. 
+#'  
+#' All functions first calculate the transition probability matrix (TPM), by calling one of the functions described under \code{\link{bcdmat}}. See that help page for more details.
+#' 
+#' @return 
+#' 
+
+# For `pivec`,  an $M$-length vector with stationary/asymptotic visit frequencies.
 #' 
 #' 
 #' @note When using the k-in-a-row design, set `matfun = kmatMarg`, not `kmatFull`.
 #' 
 #' @param cdf vector of positive-response probabilities at the doses
-#' @param matfun The function to calculate the TPM. Depends on the specific design; see \code{\link{bcdmat}}. Must
-
+#' @param matfun The function to calculate the TPM. Depends on the specific design; see \code{\link{bcdmat}}. For all functions except `classicmat`, user must provide auxiliary parameters via `...`.
+#' @param n For `currentvec, cumulvec`, at what step (= after how many observations) in the experiment would you like the vector calculated?
+#' @param startdose For `currentvec, cumulvec`, where does the experiment start? To be given as a dose-level index between 1 and \eqn{M}. If left as `NULL` (default), function will assume the equivalent of *"fair die roll"* among all doses. User can also specify your own \eqn{M}-length probability vector.
+#' @param proportions Logical (`cumulvec` only) Would you like the results returned as proportions (= a probability vector; `TRUE`, default), or as cumulative allocation counts? 
+#' @param exclude Integer (`cumulvec` only) Should the cumulative distribution exclude a certain number of initial allocations? Default 0. 
+#' 
+#' #' 
 #' @author Assaf P. Oron \code{<assaf.oron.at.gmail.com>}	  
 #' @export
 
 
-pivec<-function(cdf, matfun, ...)
+#' @references 
+#'  - Diaconis P, Stroock P. Geometric Bounds for Eigenvalues of Markov Chains. *Ann. Appl. Probab.* 1991;1(1):36-61. 
+#'  - Hughes BD. *Random Walks and Random Environments, Vol. 1.* Oxford University Press, 1995.
+
+########################## Whew! Now the actual functions.
+
+
+pivec <- function(cdf, matfun, ...)
 {
-  imat=matfun(cdf, ...) # matfun should take care of validations
-  m=length(cdf)
+  imat = matfun(cdf, ...) # matfun should take care of validations
+  m = length(cdf)
   
-  vout=cumprod(c(1,imat[cbind(1:(m-1),2:m)]/imat[cbind(2:m,1:(m-1))]))
-  vout/sum(vout)
+# Pi is determined by the ratios between up/down movements,
+#     represented as the TPM's two immediate off-diagonals
+  vout = cumprod(c(1, imat[cbind(1:(m-1), 2:m)] / imat[cbind(2:m, 1:(m-1))] ))
+# Normalizing to make it a probability vector
+    return(vout / sum(vout))
 }
 
 ######### Current allocation freq at patient n
@@ -29,7 +57,7 @@ pivec<-function(cdf, matfun, ...)
 #' @rdname pivec
 #' @export
 #' 
-currentvec <- function(startdose=NULL, cdf, n, matfun, ...)
+currentvec <- function(cdf, matfun, n, startdose = NULL, ...)
 {
   require(expm)
   checkCDF(cdf)
@@ -44,7 +72,7 @@ currentvec <- function(startdose=NULL, cdf, n, matfun, ...)
   }
 # Now some validation 
   if(any(startdose<0) || sum(startdose) != 1 || length(startdose)!=m) 
-    stop("'startdose' must be a single dose or a probability vector over doses.\n")
+    stop("'startdose' must be a single dose level, or a probability vector over doses.\n")
 
 ### Then, the actual function is just a one-liner :)
   return( vec0 %*% (matfun(cdf = cdf,...) %^% (n-1)) )
@@ -55,18 +83,19 @@ currentvec <- function(startdose=NULL, cdf, n, matfun, ...)
 #' @rdname pivec
 #' @export
 
-cumulvec<-function(startdose = NULL, cdf, n, matfun, average = TRUE, exclude = 0,...)
+cumulvec <- function(cdf, matfun, n, startdose = NULL, proportions = TRUE, exclude = 0, ...)
 {
   require(expm)
   checkCDF(cdf)
   m=length(cdf)
+  if(exclude != round(exclude) || exclude < 0) stop("'exclude' should be a non-negative integer.\n")
 
 ### Starting vector
   # The NULL default sets a uniform starting vector
-  if(is.null(startdose)) vec0=rep(1/m,m)
+  if(is.null(startdose)) vec0 = rep(1/m, m)
   if (startdose %in% 1:m) {
-    vec0=rep(0,m)
-    vec0[startdose]=1
+    vec0 = rep(0, m)
+    vec0[startdose] = 1
   }
   # Now some validation 
   if(any(startdose<0) || sum(startdose) != 1 || length(startdose)!=m) 
@@ -74,17 +103,16 @@ cumulvec<-function(startdose = NULL, cdf, n, matfun, average = TRUE, exclude = 0
   
   progmat = matfun(cdf, ...) 
   
-  ovec=rep(0,m)
-  if (is.null(exclude)) exclude=0
-  for (a in exclude:(n-1)) ovec=ovec+vec0 %*% (progmat %^% a)
-  if(average) ovec=ovec/(n-exclude)
+# The output vector  
+  ovec = rep(0, m)
+  for ( a in exclude:(n-1) ) ovec = ovec + vec0 %*% (progmat %^% a)
+# Proportions or counts?
+  if(proportions) ovec = ovec / (n - exclude)
   
-  ovec
+  return(ovec)
 }
 
 
 
 
-
-# For `pivec`,  an $M$-length vector with stationary/asymptotic visit frequencies.
 
